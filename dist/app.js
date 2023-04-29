abcc6c988eae9f27931dcbf5b2ce6580a75c767e
MichaelBrandon-UDel <70721605+MichaelBrandon-UDel@users.noreply.github.com>
2023-04-29 19:09:59 -0400

    Rethreading scripts

--- dist/app.js
+++ dist/app.js
@@ -6,14 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-function event() {
-    app.get('/', (req, res) => {
-        res.send('HenHacks 2023!');
-        return "HenHacks 2023";
-    });
-}
+app.get('/', (req, res) => {
+    res.send('HenHacks 2023!');
+});
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
-module.exports = event;
 //# sourceMappingURL=app.js.map
